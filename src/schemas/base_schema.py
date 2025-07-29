ff2da2260b85db36e6f5442798bc98d3396821b7
guido <guidomitolo@gmail.com>
2025-07-29 22:17:59 +0000

    fk tunning pending

--- src/schemas/base_schema.py
+++ src/schemas/base_schema.py
@@ -4,12 +4,15 @@ from ..database.manager import Operator
 
 
 
-class Model(BaseModel, extra='allow'):
+class Model(BaseModel):
 
-    sql_manager:ClassVar = Operator
+    _sql_manager: ClassVar = Operator
+    _db_table: ClassVar[str] = ''
+    _db_fks: ClassVar[dict] = {}
+    _db_fields: ClassVar[list] = ['id', 'created_at']
 
     def update(self, data:dict) -> None:
-        headers, row = self.sql_manager(self.Meta.__db_table__).update_record(self.id, data)
+        headers, row = self._sql_manager(self._db_table).update_record(self.id, data)
         updated_fields = self.model_fields_set.intersection(set(data.keys()))
         data = dict(zip(headers, row))
         for field in updated_fields:
@@ -17,38 +20,38 @@ class Model(BaseModel, extra='allow'):
 
     def create(self,) -> BaseModel:
         new_entity = self.model_dump(exclude_none=True, include=self.model_fields_set)
-        headers, row = self.sql_manager(self.Meta.__db_table__).create(new_entity)
+        headers, row = self._sql_manager(self._db_table).create(new_entity)
         return self.__class__(**dict(zip(headers, row)))
 
     def remove(self) -> None:
-        self.sql_manager(self.Meta.__db_table__).delete(self.id)
+        self._sql_manager(self._db_table).delete(self.id)
 
     @model_validator(mode='after')
     def check_fks(self,) -> 'Model':
-        if hasattr(self.Meta, '__db_fks__'):
-            for fk, model in self.Meta.__db_fks__.items():
-                table, id = fk.split("_")
-                headers, row = self.sql_manager(table).retrieve(getattr(self, fk))
-                fk_obj = model(**dict(zip(headers, row)))
-                setattr(self, table, fk_obj)
+        if self._db_fks:
+            headers, row = self._sql_manager(self._db_table).select(self.id, self._db_fks.keys())
+            # fk_obj = model(**dict(zip(headers, row)))
+            # setattr(self, table, fk_obj)
+            # for fk, model in self.Meta._db_fks__.items():
+            #     table, id = fk.split("_")
+            #     headers, row = self._sql_manager(table).retrieve(getattr(self, fk))
+            #     fk_obj = model(**dict(zip(headers, row)))
+            #     setattr(self, table, fk_obj)
         return self
-
+    
     @classmethod
     def get(cls, id:int) -> 'Model':
-        headers, row = cls.sql_manager(cls.Meta.__db_table__).retrieve(id)
+        headers, row = cls._sql_manager(cls._db_table).select(id, cls._db_fks.keys())
         return cls(**dict(zip(headers, row)))
     
     @classmethod
     def list(cls,) -> list['Model']:
         result_list = []
-        headers, rows = cls.sql_manager(cls.Meta.__db_table__).select()
+        headers, rows = cls._sql_manager(cls._db_table).select()
         for row in rows:
             obj = cls(**dict(zip(headers, row)))
             result_list.append(obj)
         return result_list
-
-    class Meta:
-        __db_table__ = ''
-        __db_fks__ = {}
-        __db_fields__ = ['id', 'created_at']
-
+    
+    class Config:
+        extra = "allow"
